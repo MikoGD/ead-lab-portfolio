051ec17b31d4374096e7137387a1d37c0a910fa5
Mikael Escolin <mikaelescolin@gmail.com>
2022-02-11 15:04:54 +0000

    Add utility func to check for strings

---
+++ src/utilities/index.ts
@@ -0,0 +1,4 @@
+import isString from './type-checker';
+
+export { isString } from './type-checker';
+export default isString;
---
+++ src/utilities/type-checker.ts
@@ -0,0 +1,5 @@
+export function isString(value: unknown) {
+  return typeof value === 'string' || value instanceof String;
+}
+
+export default isString;
